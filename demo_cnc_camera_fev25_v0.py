cd375b1cba17ed108013834be21d8535487bc63f
A-malta <malta.a.aline@gmail.com>
2025-02-25 18:01:19 -0300

    demo performance and interface
    correções de problemas relacionados a sincronização de frames e armazenagem; otimização de tempo de captura; interface e mais...

--- demo_cnc_camera_fev25_v0.py
+++ demo_cnc_camera_fev25_v0.py
@@ -9,21 +9,22 @@ Autor= Andre Luiz de Freitas Coelho
 Data = 20 / 02 /2025
 """
 
-#pacotes
-import cv2 as cv 
-import serial    
+# pacotes
+import cv2 as cv
+import serial
 import time
 import os
 import json
 
-#configura o diretorio para salvar
-dir_img="TT1/"
-if not os.path.exists(dir_img): os.makedirs(dir_img)
+# configura o diretorio para salvar
+dir_img = "TT1/"
+if not os.path.exists(dir_img):
+    os.makedirs(dir_img)
 
-#configuracoes do usuario
+# configuracoes do usuario
 with open("cfg.json", "r") as file:
     data = json.load(file)
-    
+
 PORT = data["port"]        # Porta de comunicação com Arduino/GRBL
 BAUDRATE = data["baudrate"]     # Taxa de transferência - padrão GRBL
 
@@ -42,18 +43,18 @@ POS_Y_PLANT = [plant["Y"] for plant in data["plants"]]
 
 
 # Inicia camera
-print ("Iniciando Camera...")
-cam=cv.VideoCapture(0)
+print("Iniciando Camera...")
+cam = cv.VideoCapture(0)
 if not cam.isOpened():
     print("Erro ao abrir a câmera. Verifique a conexão..")
 
-#configura resolucao camera
-cam.set(3,1920)#largura
-cam.set(4,1080)#altura
+# configura resolucao camera
+cam.set(3, 1920)  # largura
+cam.set(4, 1080)  # altura
 
 
 # Inicializa a comunicação serial com GRBL
-print ("Iniciando comunicacao GRBL...")
+print("Iniciando comunicacao GRBL...")
 try:
     grbl = serial.Serial(PORT, BAUDRATE, timeout=1)
     time.sleep(2)  # Aguarda inicialização do GRBL
@@ -63,88 +64,93 @@ try:
     print("Conectado ao GRBL na porta  : ", PORT)
 
 except Exception:
-    print("Erro ao conectar na porta : ",PORT)
+    print("Erro ao conectar na porta : ", PORT)
     exit()
 
 
 # funcao para enviar comando GRBL e aguardar resposta
-def send_grbl (cmd):
+def send_grbl(cmd):
     grbl.write((cmd + "\r\n").encode())  # Envia comando
     time.sleep(0.1)  # Pequeno delay para resposta
     while grbl.inWaiting() > 0:
         response = grbl.readline().decode().strip()  # Lê resposta do GRBL
         print("GRBL:", response)
 
-#espera usuario para continuar
-def wait_user (msg):
-    cmd = input('\n'+msg + " ( y= yes  n= no ) >> ") 
+# espera usuario para continuar
+
+
+def wait_user(msg):
+    cmd = input('\n'+msg + " ( y= yes  n= no ) >> ")
     if cmd.lower() == "n":
         grbl.close()
         exit()
 
-#obtem imagem
+# obtem imagem
+
+
 def GetImage(plant):
-    _,frame=cam.read()
-    frame_resized = cv.resize(frame, (640, 360))  # Reduz a resolução para exibição
-    cv.imshow('Imagem',frame_resized)
-    nome=dir_img+ID_PLANT[plant]+'.jpg' #nome do arquivo
-    cv.imwrite(nome, frame) #grava imagem
-    cv.waitKey(30) & 0xff #comando obrigatorio para o imshow
-    
-#Mostra para verificar se camera está funcionando
+    _, frame = cam.read()
+    # Reduz a resolução para exibição
+    frame_resized = cv.resize(frame, (640, 360))
+    cv.imshow('Imagem', frame_resized)
+    nome = dir_img+ID_PLANT[plant]+'.jpg'  # nome do arquivo
+    cv.imwrite(nome, frame)  # grava imagem
+    cv.waitKey(30) & 0xff  # comando obrigatorio para o imshow
+
+
+# Mostra para verificar se camera está funcionando
 GetImage(0)
 
-#comando Unlock
-#wait_user('Unlock ?')
-print ("Desbloqueando...")
+# comando Unlock
+# wait_user('Unlock ?')
+print("Desbloqueando...")
 send_grbl('$X')
 time.sleep(2)
 
-#comando ToHome
-#wait_user('To Home ?')
-print ("Go to home...")
+# comando ToHome
+# wait_user('To Home ?')
+print("Go to home...")
 send_grbl('$H')
 time.sleep(10)
 
-#Comando ir ao ponto X0 Y0 -- Precisa começar em X0 Y0????
-#wait_user('To X0 Y0 ? ')
-print ("Go to X0 Y0...")
+# Comando ir ao ponto X0 Y0 -- Precisa começar em X0 Y0????
+# wait_user('To X0 Y0 ? ')
+print("Go to X0 Y0...")
 send_grbl('G0X0Y0')
 time.sleep(10)
 
-#comando mostra configuracoes GRBL
-print ("Configuracoes GRBL...")
+# comando mostra configuracoes GRBL
+print("Configuracoes GRBL...")
 send_grbl('$')
 
-#comando mostra status da maquina
-print ("Status Maquina...")
+# comando mostra status da maquina
+print("Status Maquina...")
 send_grbl('?')
 
 
-#inicia captura automatizada 
-#Vai até cada planta e captura 1 imagem
-#wait_user('Iniciar Captura Automatica? ')
-print ("Iniciando Captura...")
+# inicia captura automatizada
+# Vai até cada planta e captura 1 imagem
+# wait_user('Iniciar Captura Automatica? ')
+print("Iniciando Captura...")
 
-#define velocidade de deslocamento
-print ("Configurando velocidade...")
-send_grbl('G1 F7000') # precisa do G1 ?????
+# define velocidade de deslocamento
+print("Configurando velocidade...")
+send_grbl('G1 F7000')  # precisa do G1 ?????
 
-#Loop para as 12 plantas
+# Loop para as 12 plantas
 for plant in range(12):
-    print ("Planta " + ID_PLANT[plant])
-    print ('Deslocando...')
+    print("Planta " + ID_PLANT[plant])
+    print('Deslocando...')
     send_grbl('G1 X'+str(POS_X_PLANT[plant])+'Y'+str(POS_Y_PLANT[plant]))
-    time.sleep(10) #aguarda chegar
-    GetImage(plant) #obtem imagem para a planta
-    #if plant < 11: wait_user('Proxima planta? ')
+    time.sleep(10)  # aguarda chegar
+    GetImage(plant)  # obtem imagem para a planta
+    # if plant < 11: wait_user('Proxima planta? ')
 
-#Retorna para X0 Y0
-#wait_user('To X0 Y0 ? ')
-print ("Go to X0 Y0...")
+# Retorna para X0 Y0
+# wait_user('To X0 Y0 ? ')
+print("Go to X0 Y0...")
 send_grbl('G0X0Y0')
 time.sleep(10)
 
 print("Fechando conexão...")
 grbl.close()
-
