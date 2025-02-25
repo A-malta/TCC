e1c2dc247853de13452aed47c52dcd6e7a721926
A-malta <malta.a.aline@gmail.com>
2025-02-25 08:05:38 -0300

    Update demo_cnc_camera_fev25_v0.py

--- demo_cnc_camera_fev25_v0.py
+++ demo_cnc_camera_fev25_v0.py
@@ -10,23 +10,18 @@ Data = 20 / 02 /2025
 """
 
 #pacotes
-import cv2 as cv 
-import serial    
+import cv2 as cv #conda install conda-forge::opencv
+import serial    #pip install pyserial
 import time
 import os
-import json
 
 #configura o diretorio para salvar
 dir_img="TT1/"
 if not os.path.exists(dir_img): os.makedirs(dir_img)
 
 #configuracoes do usuario
-with open("cfg.json", "r") as file:
-    data = json.load(file)
-    
-PORT = data["port"]        # Porta de comunicação com Arduino/GRBL
-BAUDRATE = data["baudrate"]     # Taxa de transferência - padrão GRBL
-
+PORT = "COM3"    # porta de comunicacão com Arduino
+BAUDRATE = 115200 # taxa de transferencia - padrão GRBL
 '''
  P1 a P6 direita , P7 a P12 direita
 <---- -X
@@ -35,14 +30,14 @@ P3 P4
 ....
 P11 P12
 '''
-ID_PLANT = [plant["id"] for plant in data["plants"]]
-POS_X_PLANT = [plant["X"] for plant in data["plants"]]
-POS_Y_PLANT = [plant["Y"] for plant in data["plants"]]
+ID_PLANT= ['P1','P2','P3','P4','P5','P6','P7','P8','P9','P10','P11','P12']
+POS_X_PLANT = [-300,-300,-300,-300,-300,-300,-600,-600,-600,-600,-600,-600] 
+POS_Y_PLANT = [-300,-300,-600,-600,-900,-900,-1200,-1200,-1500,-1500,-1800,-1800] 
 
 
 # Inicia camera
 print ("Iniciando Camera...")
-cam=cv.VideoCapture(0)
+cam=cv.VideoCapture(1)
 if not cam.isOpened():
     print("Erro ao abrir a câmera. Verifique a conexão..")
 
@@ -94,19 +89,19 @@ def GetImage(plant):
 GetImage(0)
 
 #comando Unlock
-#wait_user('Unlock ?')
+wait_user('Unlock ?')
 print ("Desbloqueando...")
 send_grbl('$X')
 time.sleep(2)
 
 #comando ToHome
-#wait_user('To Home ?')
+wait_user('To Home ?')
 print ("Go to home...")
 send_grbl('$H')
 time.sleep(10)
 
 #Comando ir ao ponto X0 Y0 -- Precisa começar em X0 Y0????
-#wait_user('To X0 Y0 ? ')
+wait_user('To X0 Y0 ? ')
 print ("Go to X0 Y0...")
 send_grbl('G0X0Y0')
 time.sleep(10)
@@ -122,7 +117,7 @@ send_grbl('?')
 
 #inicia captura automatizada 
 #Vai até cada planta e captura 1 imagem
-#wait_user('Iniciar Captura Automatica? ')
+wait_user('Iniciar Captura Automatica? ')
 print ("Iniciando Captura...")
 
 #define velocidade de deslocamento
@@ -136,10 +131,10 @@ for plant in range(12):
     send_grbl('G1 X'+str(POS_X_PLANT[plant])+'Y'+str(POS_Y_PLANT[plant]))
     time.sleep(10) #aguarda chegar
     GetImage(plant) #obtem imagem para a planta
-    #if plant < 11: wait_user('Proxima planta? ')
+    if plant < 11: wait_user('Proxima planta? ')
 
 #Retorna para X0 Y0
-#wait_user('To X0 Y0 ? ')
+wait_user('To X0 Y0 ? ')
 print ("Go to X0 Y0...")
 send_grbl('G0X0Y0')
 time.sleep(10)
